30c481ea7ed38bd8e36c274fb126382b9df3eec4
dudeperf3ct <dudeperf3ct@users.noreply.github.com>
2021-03-01 19:15:01 +0000

    add missing import

--- fastapi/image_classifier_router.py
+++ fastapi/image_classifier_router.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter,File, UploadFile
+from fastapi import APIRouter,File, UploadFile, Form
 from model import FMIModel
 from starlette.responses import StreamingResponse, JSONResponse
 import numpy as np
