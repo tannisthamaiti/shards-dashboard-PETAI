7970487c924b0efa1bd553ca30db545f50639e22
AnindaBhattacharjee <anindabhattacharjee123@gmail.com>
2021-03-02 10:55:22 +0100

    get method to return img file from dir

--- fastapi/app.py
+++ fastapi/app.py
@@ -1,6 +1,7 @@
 from sys import prefix
 import image_classifier_router
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
 
 app = FastAPI()
 app.include_router(image_classifier_router.router, prefix="/Image")
@@ -8,3 +9,7 @@ app.include_router(image_classifier_router.router, prefix="/Image")
 @app.get('/healthcheck', status_code=200)
 async def healthcheck():
     return 'dummy check! classifier  is all ready to go!'
+
+@app.get("/image_return/{file}")
+async def img_return(file :str):
+    return FileResponse("outputs/"+file+".png")
